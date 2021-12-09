9ed42f47323e6de3917fc5d9a629686cd90b7e54
Alex <88716331+Fressmuxer@users.noreply.github.com>
2021-12-09 12:47:27 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,15 +4,15 @@
 sudo mdutil -i off -a
 
 #Create new account
-sudo dscl . -create /Users/vncuser
-sudo dscl . -create /Users/vncuser UserShell /bin/bash
-sudo dscl . -create /Users/vncuser RealName "VNC User"
-sudo dscl . -create /Users/vncuser UniqueID 1001
-sudo dscl . -create /Users/vncuser PrimaryGroupID 80
-sudo dscl . -create /Users/vncuser NFSHomeDirectory /Users/vncuser
-sudo dscl . -passwd /Users/vncuser $1
-sudo dscl . -passwd /Users/vncuser $1
-sudo createhomedir -c -u vncuser > /dev/null
+sudo dscl . -create /Users/fressmix
+sudo dscl . -create /Users/fressmix UserShell /bin/bash
+sudo dscl . -create /Users/fressmix RealName "Fressmix"
+sudo dscl . -create /Users/fressmix UniqueID 1001
+sudo dscl . -create /Users/fressmix PrimaryGroupID 80
+sudo dscl . -create /Users/fressmix NFSHomeDirectory /Users/fressmix
+sudo dscl . -passwd /Users/fressmix $1
+sudo dscl . -passwd /Users/fressmix $1
+sudo createhomedir -c -u fressmix > /dev/null
 
 #Enable VNC
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
